413a9239e5226cf9087da9021f91ae3d0c03240c
TanThanhLe <tanthanhcr95@gmail.com>
2024-05-05 17:15:00 +0700

    Steps for reading a text file in Python

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 if __name__ == "__main__":
-  print("Python Read Text File")
+  # Steps for reading a text file in Python
+
+  """
+  To read a text file in Python, you follow these steps:
+
+  - First, open a text file for reading by using the open() function.
+
+  - Second, read text from the text file using the read(), readline() or readlines() method of the file object.
+
+  - Third, close the file using the file close() method.
+  """
